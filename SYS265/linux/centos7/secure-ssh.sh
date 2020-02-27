4ab44f50e869a509760a16be6b9e2bb41c20867b
jturner2000 <jturner2000@jessica.turner.mymail.champlain.edu>
2020-02-27 04:41:47 +0000

    secure-ssh.sh script updated

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,10 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo "Creating script with user [$1"]
+useradd -m -d /home/$1 -s /bin/bash $1
+mkdir /home/$1/.ssh
+cp ../public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
